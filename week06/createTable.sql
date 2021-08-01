4db38e8b850b72d4df9d0ee7f98af2a71774abc0
TigerNingGithub <365176094@qq.com>
2021-08-01 20:18:44 +0800

    第六周作业

---
+++ week06/createTable.sql
@@ -0,0 +1,56 @@
+﻿-- 2、（必做）：基于电商交易场景（用户、商品、订单），设计一套简单的表结构，提交 DDL 的
+-- SQL文件到Github（后面2周的作业依然要是用到这个表结构）。
+
+
+--## 创建数据库
+create database `shops` default character set utf8mb4 collate utf8mb4_general_ci;
+
+drop table `users`
+
+--##用户表
+create table if not exists `users`(
+`id` int(11) not null auto_increment,
+`name` varchar(255) not null comment '用户名',
+`password` varchar(255) not null comment '密码',
+`nick_name` varchar(255) not null comment '昵称',
+`identify_card` varchar(18) not null comment '身份证',
+PRIMARY KEY(`id`)
+)engine=innodb AUTO_INCREMENT=1 DEFAULT CHARSET=utf8mb4;
+
+
+--##供应商 拆解商品表
+create table if not exists `supplier`(
+`id` int(11) not null auto_increment,
+`name` varchar(255) not null comment '名称',
+`registration_number` varchar(255) not null comment '注册号',
+`contactPerson` varchar(255) not null comment '联系人',
+PRIMARY KEY(`id`)
+)engine=innodb AUTO_INCREMENT=1 DEFAULT CHARSET=utf8mb4;
+
+
+
+--##商品
+create table if not exists `goods`(
+`id` int(11) not null auto_increment,
+`name` varchar(255) not null comment '名称',
+`classify` varchar(255) not null comment '分类',
+`weight` double not null comment '重量',
+`supplier_id` int(11) not null comment '供应商编码',
+PRIMARY KEY(`id`),
+ foreign key (supplier_id) references supplier(id)
+)engine=innodb AUTO_INCREMENT=1 DEFAULT CHARSET=utf8mb4;
+
+
+
+--##订单
+create table if not exists `orders`(
+`id` int(11) not null auto_increment,
+`user_id` int(11) not null comment '用户ID',
+`money` double not null comment '金额',
+`state` int(11) not null comment '状态 存储枚举',
+`goods_id` int(11) not null comment '商品编码',
+PRIMARY KEY(`id`),
+foreign key (user_id) references users(id),
+foreign key (goods_id) references goods(id)
+)engine=innodb auto_increment=1 DEFAULT charset=utf8mb4;
+
---
+++ week06/initDataByProcedure.sql
@@ -0,0 +1,76 @@
+﻿-- ###批量插入数据
+-- # 插入数据到用户表中
+DROP PROCEDURE IF EXISTS users_initData;
+DELIMITER $  
+CREATE PROCEDURE users_initData()
+BEGIN
+
+    DECLARE n INT DEFAULT 1; 
+    WHILE n<=100 DO       
+        insert into shops.users (name, password, nick_name, identify_card)
+        VALUES (CONCAT("user", n), "password", CONCAT("nick_name", n), "identify_card");
+        SET n = n+1;
+    END WHILE;
+    commit;
+END $
+CALL users_initData();
+
+select * from shops.users;
+
+-- # 插入数据到供应商表中
+
+DROP PROCEDURE IF EXISTS supplier_initData;
+DELIMITER $  
+CREATE PROCEDURE supplier_initData()
+BEGIN
+
+    DECLARE n INT DEFAULT 1; 
+    WHILE n<=100 DO      
+        insert into shops.supplier (name, registration_number, contactPerson)
+        VALUES (CONCAT("supplierName", n), "registration_number", CONCAT("contactPerson", n));
+        SET n = n+1;
+    END WHILE;
+    commit;
+END $
+CALL supplier_initData();
+
+select * from shops.supplier;
+
+-- ###商品
+=name,classify,weight,supplier_id
+
+DROP PROCEDURE IF EXISTS goods_initData;
+DELIMITER $  
+CREATE PROCEDURE goods_initData()
+BEGIN
+
+    DECLARE n INT DEFAULT 1; 
+    WHILE n<=100 DO      
+        insert into shops.goods (name, classify, weight,supplier_id)
+        VALUES (CONCAT("goodName", n), "classify", n,n);
+        SET n = n+1;
+    END WHILE;
+    commit;
+END $
+CALL goods_initData();
+
+select * from  goods;
+
+-- ###订单
+-- =user_id,money,state,goods_id
+DROP PROCEDURE IF EXISTS orders_initData;
+DELIMITER $  
+CREATE PROCEDURE orders_initData()
+BEGIN
+
+    DECLARE n INT DEFAULT 1; 
+    WHILE n<=100 DO       
+        insert into shops.orders (user_id, money, state,goods_id)
+        VALUES (n, n+1, n+2,n);
+        SET n = n+1;
+    END WHILE;
+    commit;
+END $
+CALL orders_initData();
+
+select * from  orders;
